efce5c79603b5f08289565744593a2c2d105419e
Haseeb Khan <156788219+Haseeb21k@users.noreply.github.com>
2024-01-23 11:40:50 +0000

    update

--- js_prac/basics.js
+++ js_prac/basics.js
@@ -1,1 +1,8 @@
-console.log("Hsb")
+const name = "Hsb"
+let item = 20
+console.log(`Hello my name is ${name}`);
+console.log(name[2]);
+console.log(name.length);
+const url = "www.google.com/w3schools20%javascript/mdn20%js"
+console.log(url.replace("20%","--"));
+console.log(url.split("/"));
