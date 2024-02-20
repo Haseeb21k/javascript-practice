c7b54e2457f2a176e750b62ed6a383c24bb4f84e
Haseeb Khan <156788219+Haseeb21k@users.noreply.github.com>
2024-02-20 07:46:31 +0000

    update

--- js_prac/basics.js
+++ js_prac/basics.js
@@ -1,8 +1,32 @@
 const name = "Hsb"
 let item = 20
-console.log(`Hello my name is ${name}`);
-console.log(name[2]);
-console.log(name.length);
-const url = "www.google.com/w3schools20%javascript/mdn20%js"
-console.log(url.replace("20%","--"));
-console.log(url.split("/"));
+// console.log(`Hello my name is ${name}`);
+// console.log(name[2]);
+// console.log(name.length);
+// const url = "www.google.com/w3schools20%javascript/mdn20%js"
+// console.log(url.replace("/"," "));
+// console.log(url.split("/"));
+
+// const mydate = new Date
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toString());
+// console.log(mydate.toISOString());
+// console.log(mydate.getDay());
+// const timestamp = Date.now()
+// console.log(Math.floor(timestamp/1000));
+
+
+const new_array = [2, 4, 6, 8, 10, 12]
+const second_array = [1, 3, 5, 7, 9, 11]
+// console.log(new_array);
+// console.log(new_array.length);
+// const conc_arr = [new_array.concat(second_array)]
+//console.log(conc_arr);
+// console.log(new_array.push(13));
+// console.log(Array.isArray(new_array));
+// console.log(new_array.toString());
+// new_array.push(second_array)
+// console.log(new_array.flat());
+
